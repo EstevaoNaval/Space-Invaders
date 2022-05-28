e5d30ec7342c9a23059c02b04c0ce695263810ec
EstevaoNaval <navalestevao@gmail.com>
2022-05-27 23:26:03 -0300

    Reiniciando projeto com PPlay como framework

--- src/player.py
+++
@@ -1,7 +0,0 @@
-from importlib.resources import path
-from PPlay.sprite import *
-from settings import *
-
-path_nave = "./asset/nave/nave.png"
-nave = Sprite(path_nave, 1)
-nave.set_position((WIDTH - nave.width)/2, (HEIGHT - nave.height))
