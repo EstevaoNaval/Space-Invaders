aef29fe52c5013c718605965efa72061f86426c3
Estevão Naval <navalestevao@gmail.com>
2022-06-03 10:51:48 -0300

    Subindo set do monstros

--- src/main.py
+++ src/main.py
@@ -4,16 +4,26 @@ from PPlay.gameobject import *
 from PPlay.keyboard import *
 from PPlay.sprite import *
 from settings import *
+from pygame import time
 
 #def move_nave(keycode, nave):
 #    if(keycode.key_pressed["LEFT"]):
 #        nave.move_key_x(4)
 
-def draw(nave, bullets):
+def draw(nave, matrix_invader, num_row_invader, num_column_invader, bullets):
     nave.draw()
+
+    for row in range(num_row_invader):
+        for column in range(num_column_invader):
+            matrix_invader[row][column].draw()
 
     for bullet in bullets:
         bullet.draw()
+
+def draw_fps(fps, list_position, size, color, font_name):
+    int_fps_2_string = str(int(fps))
+    janela.draw_text("FPS: " + int_fps_2_string, list_position[0], list_position[1], size, color, font_name)
+
 
 def move_bullets(bullets, janela):
     nave_bullet_direction = -1
@@ -31,6 +41,27 @@ def set_bullet_position(bullet, atirador):
     bullet.x = shoot_x
     bullet.y = shoot_y
 
+def set_nave(path_nave):
+    nave = Sprite(path_nave, 1)
+    nave.set_position((WIDTH - nave.width)/2, (HEIGHT - nave.height))
+
+    return nave
+
+def set_invander(path_invader, row, column):
+    invader = Sprite(path_invader, 1)
+    x, y = row * invader.width, column * invader.height
+    invader.set_position(x, y)
+    
+    return invader
+
+def set_matrix_invader(path_invader):
+    matrix_invaders = [[0] * NUM_COLUMN_INVADER] * NUM_ROW_INVADER
+
+    for row in range(NUM_ROW_INVADER):
+        for  column in range(NUM_COLUMN_INVADER):
+            matrix_invaders[row][column] = set_invander(path_invader, row, column)
+    
+    return matrix_invaders
 
 def shoot(atirador, path_bullet):
     shoot_tick = 0
@@ -43,24 +74,27 @@ def shoot(atirador, path_bullet):
 
     return shoot_tick
 
-def handle_nave_collision(nave, janela):
+def handle_nave_collision(nave):
     if nave.x < 0: nave.x = 0
     elif nave.x + nave.width > WIDTH: nave.x = WIDTH - nave.width
-
+    
 janela = Window(WIDTH, HEIGHT)
 janela.set_title(TITLE)
+
+clock = time.Clock()
 
 path_bg = "./assets/background/bg.png"
 fundo = GameImage(path_bg)
 
 key = Keyboard()
 
-path_nave = "./assets/nave/nave.png"
-nave = Sprite(path_nave, 1)
-nave.set_position((WIDTH - nave.width)/2, (HEIGHT - nave.height))
+nave = set_nave("./assets/nave/nave.png")
 
 path_bullet = "./assets/bullet/pixel_laser_black.png"
 bullets = []
+
+path_invader = "./assets/invader/invader_01.png"
+matrix_invaders = set_matrix_invader(path_invader)
 
 enemy_shoot_delay = 1/GAME_SPEED 
 shoot_delay = 1/GAME_SPEED * 0.5
@@ -69,13 +103,14 @@ shoot_tick = shoot_delay
 running = 1
 
 while running:
-    janela.set_background_color(DARKGREY)
+    clock.tick(FPS)
     
+    janela.set_background_color(DARKGREY)
     fundo.draw()
 
     shoot_tick += janela.delta_time()
 
-    handle_nave_collision(nave, janela)
+    handle_nave_collision(nave)
 
     if(key.key_pressed("LEFT")): nave.move_x(-NAVE_SPEED * janela.delta_time())
     elif(key.key_pressed("RIGHT")): nave.move_x(NAVE_SPEED * janela.delta_time())
@@ -86,6 +121,9 @@ while running:
     
     move_bullets(bullets, janela)
 
-    draw(nave, bullets)
+    draw(nave, matrix_invaders, NUM_ROW_INVADER, NUM_COLUMN_INVADER, bullets)
+    draw_fps(clock.get_fps(), [0,0], 20, BLACK, FONT_NAME)
+    
+    
 
     janela.update()
